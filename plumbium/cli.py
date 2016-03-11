1b2a58455805963022795ce10129450f23504b58
Jon Stutters <j.stutters@ucl.ac.uk>
2016-03-11 14:05:56 +0000

    Add debug option to cli

--- plumbium/cli.py
+++ plumbium/cli.py
@@ -5,20 +5,24 @@ from processresult import recorder
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--debug/--no-debug', default=False)
+@click.pass_context
+def cli(ctx, debug):
+    ctx.obj = {'DEBUG': debug}
 
 
 @cli.command()
 @click.argument('t1', type=click.Path(exists=True, dir_okay=False))
 @click.argument('t2', type=click.Path(exists=True, dir_okay=False))
 @click.argument('t2_lesions', type=click.Path(exists=True, dir_okay=False))
-def fill_and_extract_brain(t1, t2, t2_lesions):
+@click.pass_context
+def fill_and_extract_brain(ctx, t1, t2, t2_lesions):
     recorder.run(
         'fill_and_extract_brain',
         pipelines.fill_and_extract_brain,
         Image(t1),
-        Image(t2)
+        Image(t2),
+        debug=ctx.obj['DEBUG']
     )
 
 
@@ -30,7 +34,8 @@ def fill_and_extract_brain(t1, t2, t2_lesions):
 @click.argument('mtoff_short', type=click.Path(exists=True, dir_okay=False))
 @click.argument('mton_long', type=click.Path(exists=True, dir_okay=False))
 @click.argument('mtoff_long', type=click.Path(exists=True, dir_okay=False))
-def mtr(t1, t2, t2_lesions, mton_short, mtoff_short, mton_long, mtoff_long):
+@click.pass_context
+def mtr(ctx, t1, t2, t2_lesions, mton_short, mtoff_short, mton_long, mtoff_long):
     recorder.run(
         'mtr',
         pipelines.mtr,
@@ -40,17 +45,20 @@ def mtr(t1, t2, t2_lesions, mton_short, mtoff_short, mton_long, mtoff_long):
         Image(mton_short),
         Image(mtoff_short),
         Image(mton_long),
-        Image(mtoff_long)
+        Image(mtoff_long),
+        debug=ctx.obj['DEBUG']
     )
 
 
 @cli.command()
 @click.argument('input_file', type=click.Path(exists=True, dir_okay=False))
-def test(input_file):
+@click.pass_context
+def test(ctx, input_file):
     recorder.run(
         'test',
         pipelines.test,
-        Image(input_file)
+        Image(input_file),
+        debug=ctx.obj['DEBUG']
     )
 
 
