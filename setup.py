0357b2a276ae5bf988dd2e6cf89ee9cae2a14f57
Jon Stutters <j.stutters@ucl.ac.uk>
2016-03-16 13:40:09 +0000

    Remove CLI stuff, bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,19 +2,12 @@ from setuptools import setup
 
 setup(
     name='plumbium',
-    version='0.0.5',
+    version='0.1.0',
     packages=['plumbium'],
     zip_safe=True,
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
-        [console_scripts]
-        plumbium=plumbium.cli:cli
-    ''',
     author='Jon Stutters',
     author_email='j.stutters@ucl.ac.uk',
-    description='MRI image analysis tools',
+    description='Record the inputs and outputs of scripts',
     url='https://github.com/jstutters/plumbium',
     license='MIT',
     classifiers=[
