56cde7a0a93a733e0f2958837f935b1446214168
Jon Stutters <j.stutters@ucl.ac.uk>
2016-03-16 13:31:09 +0000

    Improve test coverage of artefact module

--- tests/test_artefacts.py
+++ tests/test_artefacts.py
@@ -2,11 +2,21 @@ import pytest
 from plumbium import artefacts
 
 
-def test_NiiGzImage_basename():
-    img = artefacts.NiiGzImage('foo.nii.gz')
+def test_Artefact_basename():
+    img = artefacts.Artefact('foo.nii.gz', '.nii.gz')
     assert img.basename == 'foo'
+
+
+def test_Artefact_repr():
+    img = artefacts.Artefact('foo.nii.gz', '.nii.gz')
+    assert repr(img) == "Artefact('foo.nii.gz')"
 
 
 def test_NiiGzImage_bad_extension():
     with pytest.raises(ValueError):
         img = artefacts.NiiGzImage('foo.nii.gx')
+
+
+def test_TextFile_bad_extension():
+    with pytest.raises(ValueError):
+        img = artefacts.NiiGzImage('foo.txx')
