bc0f91439725ff80c04fd17a39a395688d5deb4c
Jon Stutters <j.stutters@ucl.ac.uk>
2016-03-18 08:00:05 +0000

    Remove debug prints

--- plumbium/processresult.py
+++ plumbium/processresult.py
@@ -35,8 +35,6 @@ class Pipeline(object):
         for i in self.input_files:
             shutil.copy(i.filename, self.working_dir)
             i.dereference()
-        print(os.listdir(self.working_dir))
-        print(self.input_files)
 
     def _store_printed_output(self):
         with open('printed_output.txt', 'w') as printed_output_record:
