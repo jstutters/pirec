07b8560d28838f7f703b16f836e30b94c3940e6f
Jon Stutters <j.stutters@ucl.ac.uk>
2016-04-15 11:55:16 +0100

    Add long description and test command to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='plumbium',
     version='0.4.0',
@@ -8,7 +12,10 @@ setup(
     author='Jon Stutters',
     author_email='j.stutters@ucl.ac.uk',
     description='Record the inputs and outputs of scripts',
+    long_description=readme(),
     url='https://github.com/jstutters/plumbium',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     license='MIT',
     classifiers=[
     ]
