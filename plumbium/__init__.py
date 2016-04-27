769a7a9a9099445fda0c0ab2a623713adf585ab7
Jon Stutters <j.stutters@ucl.ac.uk>
2016-04-27 14:31:53 +0100

    Use absolute import style in __init__.py

--- plumbium/__init__.py
+++ plumbium/__init__.py
@@ -2,4 +2,5 @@
 .. moduleauthor:: Jon Stutters <j.stutters@ucl.ac.uk>
 """
 
-from processresult import pipeline, record, call
+from __future__ import absolute_import
+from .processresult import pipeline, record, call
