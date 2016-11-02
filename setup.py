99e4fe7b3fa8ad11cc747945c5dba91e33180c93
Jon Stutters <j.stutters@ucl.ac.uk>
2016-11-02 12:14:24 +0000

    Update changelog and version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='plumbium',
-    version='0.6.3',
+    version='0.6.4',
     packages=['plumbium', 'plumbium.recorders'],
     zip_safe=True,
     author='Jon Stutters',
