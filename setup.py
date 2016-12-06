a40b56bdb90d226274b868d85d86b5e9839f1d55
Jon Stutters <j.stutters@ucl.ac.uk>
2016-12-06 17:39:03 +0000

    Bump version to 0.8.0 and update changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='plumbium',
-    version='0.7.0',
+    version='0.8.0',
     packages=['plumbium', 'plumbium.recorders'],
     zip_safe=True,
     author='Jon Stutters',
