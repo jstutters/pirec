221300ca7176464a7fe4f60ccd4c479d39dc24b0
Jon Stutters <j.stutters@ucl.ac.uk>
2017-01-05 16:47:51 +0000

    Stop importing absolute_import from __future__

--- plumbium/recorders/__init__.py
+++ plumbium/recorders/__init__.py
@@ -5,7 +5,6 @@ plumbium.recorders
 Module containing functions for recording results to files and databases.
 """
 
-from __future__ import absolute_import
 from .csvfile import CSVFile
 from .sqldatabase import SQLDatabase
 from .mongodb import MongoDB
