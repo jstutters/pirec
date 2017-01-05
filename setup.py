c08e04439397742f4aaafd5dfcc6c6799bfba0a1
Jon Stutters <j.stutters@ucl.ac.uk>
2017-01-05 13:21:57 +0000

    Bump to version 0.9.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='plumbium',
-    version='0.9.0',
+    version='0.9.1',
     packages=['plumbium', 'plumbium.recorders'],
     zip_safe=True,
     author='Jon Stutters',
