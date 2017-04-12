0d091ac395961f2df4a256430e83e9513d51574f
Jon Stutters <j.stutters@ucl.ac.uk>
2017-04-12 12:57:38 +0100

    Add json_column argument to SQLDatabase recorder

--- pirec/recorders/sqldatabase.py
+++ pirec/recorders/sqldatabase.py
@@ -1,4 +1,5 @@
 """Exposes the SQLDatabase result recorder."""
+import json
 try:
     from sqlalchemy import create_engine, text
 except ImportError:
@@ -13,15 +14,19 @@ class SQLDatabase(object):
         table (str): table name
         values (dict): a mapping from database table columns to values
 
+    Keyword Args:
+        json_column (str): If supplied the complete result dictionary will be written to this column
+
     See Also:
         `SQLAlchemy documentation <http://docs.sqlalchemy.org/en/latest/core/connections.html>`_
     """
 
-    def __init__(self, uri, table, values):
+    def __init__(self, uri, table, values, json_column=None):
         """Initialize the recorder."""
         self.uri = uri
         self.table = table
         self.values = values
+        self.json_column = json_column
 
     def write(self, results):
         """Write the results to the database table specified at initialisation.
@@ -32,6 +37,9 @@ class SQLDatabase(object):
         engine = create_engine(self.uri)
         field_names = ','.join(self.values.keys())
         values_placeholder = ','.join([':{0}'.format(k) for k in self.values.keys()])
+        if self.json_column is not None:
+            field_names += ',' + self.json_column
+            values_placeholder += ',:_JSON_'
         query_string = """
             INSERT INTO {table}
             ({field_names})
@@ -45,4 +53,6 @@ class SQLDatabase(object):
         row = {}
         for field in self.values:
             row[field] = self.values[field](results)
+        if self.json_column is not None:
+            row['_JSON_'] = json.dumps(results)
         engine.execute(query, **row)
